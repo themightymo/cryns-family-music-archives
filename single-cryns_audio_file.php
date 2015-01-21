319a0679884500412d3c6efb417f6395cedc5286
Toby Cryns <toby@themightymo.com>
2015-01-20 22:34:18 -0600

    Display HTML5 Audio Player

--- single-cryns_audio_file.php
+++ single-cryns_audio_file.php
@@ -107,6 +107,17 @@ get_header(); ?>
 				Download: <a href="<?php echo wp_get_attachment_url(get_post_meta($post->ID, 'Audio File', true)); ?>"><?php the_title(); ?></a><br /><br />
 		        </span>
 		    <?php } ?>
+		    
+		    <?php
+			$mp3 = wp_get_attachment_url(get_post_meta($post->ID, 'Audio File', true)); 
+			$attr = array(
+				'src'      => $mp3,
+				'loop'     => '',
+				'autoplay' => '',
+				'preload' => 'none'
+				);
+			echo wp_audio_shortcode( $attr );
+			?>
 
 			<?php the_post_navigation(); ?>
 
