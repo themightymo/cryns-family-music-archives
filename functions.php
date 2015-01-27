dbf0bda73052e2b6099f5a5489f4e553789e22f1
Toby Cryns <toby@themightymo.com>
2015-01-26 20:07:01 -0600

    lots of style updates

--- functions.php
+++ functions.php
@@ -113,6 +113,15 @@ function cfma_scripts() {
 add_action( 'wp_enqueue_scripts', 'cfma_scripts' );
 
 /**
+ * Load Google Web Font
+ */
+function load_fonts() {
+	wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Roboto:400,900');
+	wp_enqueue_style( 'googleFonts');
+}
+add_action('wp_print_styles', 'load_fonts');
+
+/**
  * Implement the Custom Header feature.
  */
 //require get_template_directory() . '/inc/custom-header.php';
