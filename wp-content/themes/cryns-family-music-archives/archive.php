58330ad9b39cdaf3e0b9c097da6faa48cde94409
Toby Cryns <toby@themightymo.com>
2015-02-03 22:02:02 -0600

    draft of archive.php with playlist functionality

--- wp-content/themes/cryns-family-music-archives/archive.php
+++ wp-content/themes/cryns-family-music-archives/archive.php
@@ -12,35 +12,70 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+			<?php $mp3Files = array (); //Store the audio file id in an array ?>
+			
+			<?php if ( have_posts() ) : ?>
+	
+				<header class="page-header">
+					<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
+					?>
+				</header><!-- .page-header -->
+	
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+	
+					<div><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+					<?php
+					global $post;
+						if ( wp_get_attachment_url( get_post_meta($post->ID, 'Audio File', true) ) ) {
+						//$mp3 = 'hello'; 
+						/*$attr = array(
+							'src'		=> $mp3,
+							'loop'		=> '',
+							'autoplay'	=> '',
+							'preload'	=> 'none'
+						);
+						
+						//Add the mp3 id to the end of the array
+						$mp3ID = get_post_meta($post->ID, 'Audio File', true); 
+						$mp3Files[] = $mp3ID;*/
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php endif; ?>
+					}
+					?>
+	
+	
+				<?php endwhile; ?>
+	
+				<?php the_posts_navigation(); ?>
+	
+			<?php else : ?>
+	
+				<?php get_template_part( 'content', 'none' ); ?>
+	
+			<?php endif; ?>
+			
+			<?php 
+			/*$myvar = '';
+			
+			for ($index = 0; $index < count($mp3Files); $index++){  
+				$myvar .= $mp3Files[$index];
+				$myvar .= ',';
+			}
+			
+			echo do_shortcode('[playlist ids="' . $myvar . '"]');
+			
+			
+			$mp3 = wp_get_attachment_url(get_post_meta($post->ID, 'Audio File', true)); 
+			$attr = array(
+			'src'      => $mp3,
+			'loop'     => '',
+			'autoplay' => '',
+			'preload' => 'none'
+			);
+			echo wp_playlist_shortcode( $attr );*/
+			?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
