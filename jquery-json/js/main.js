b8713493e6a07bea183ee9822d4fc9412447b1c8
Toby Cryns <toby@themightymo.com>
2015-02-07 19:38:44 -0600

    Create an each loop in jquery

--- jquery-json/js/main.js
+++ jquery-json/js/main.js
@@ -1,17 +1,31 @@
 jQuery(function($) {
     $.getJSON('http://music.cryns.dev/wp-json/posts/?type=cryns_audio_file')
         .success(function(response) {
-            var $content = $('#content')
-                .html(response[0].title);
-
-            $content.find('img').each(function() {
-                var $this = $(this);
-                $this.height($this.height() / 2);
-                $this.width($this.width() / 2);
-            });
-
-            $('#title')
-                .html(response[0].title)
-                .attr('href', response[0].link);
+	        
+	        var html = '';
+	        
+	        $.each( response, function( key, value ) {
+				
+				html += '<li><a href="' + value.link + '">' + value.title + '</a></li>';
+				
+	            /*
+		        var $content = $('#content')
+	                .html(value.title);
+	
+	            $content.find('img').each(function() {
+	                var $this = $(this);
+	                $this.height($this.height() / 2);
+	                $this.width($this.width() / 2);
+	            });
+	
+	            $('#title')
+	                .html(value.title)
+	                .attr('href', value.link);
+	            */
+	            // End each    
+	        });
+	        $('#title').html(html);
         });
+        
+        
 });
