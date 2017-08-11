6f6e983aecabfb5c5120c2398dcb135fecca3ce5
Toby Cryns <toby@themightymo.com>
2017-08-11 08:43:52 -0500

    add footer credits

--- wp-content/plugins/cryns_music_taxonomies_and_custom_post.php
+++ wp-content/plugins/cryns_music_taxonomies_and_custom_post.php
@@ -411,3 +411,8 @@ function output_before_taxonomy_loop(){
    
 	}
 }
+
+function footer_credits () {
+    echo '<div style="font-size:small;text-align:center;margin-bottom:1em;">Music by the <a href="https://github.com/themightymo/cryns-family-music-archives" target="_blank">Cryns Family Music Archives</a> Plugin</div>';
+}
+add_action( 'wp_footer', 'footer_credits' );
