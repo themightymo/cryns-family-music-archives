c53e0ce7a2fef1e508c12057f2bf9eee06cbc419
Toby Cryns <toby@themightymo.com>
2021-01-21 09:28:49 -0600

    Deleted junk files, moved to root

--- wp-content/themes/cryns-music-twentytwelve-child/functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-function parent_css_theme_style() { 
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
-}
-add_action( 'wp_enqueue_scripts', 'parent_css_theme_style' );
